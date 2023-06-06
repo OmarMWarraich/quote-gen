46a149b10029eb5cd249dd6cf3ad5a05b7c4aa8f
OmarMWarraich <earthling986@gmail.com>
2023-06-06 08:44:50 +0500

    feat: App Config w/ aws-amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Amplify } from 'aws-amplify'
+import config from '../src/aws-exports'
+
+Amplify.configure({ ...config, ssr: true })
+
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
